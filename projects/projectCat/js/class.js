49e5305efba35b46421b361d01d3fd0ed80fe55b
Shirajuki <vulturegfd@reborn.com>
2019-04-14 20:47:20 +0200

    yep

---
+++ projects/projectCat/js/class.js
@@ -0,0 +1,430 @@
+//// OBJECTS
+class Object {
+  constructor(x, y, width, height, color, dx = 0, dy = 0, swap = false, r = 0, dmg = 1, img={set:sprite1,sx:91,sy:1,sw:9,sh:9}) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.dx = dx;
+    this.dy = dy;
+    this.swap = swap;
+    this.r = r;
+    this.dmg = dmg;
+    this.img = img;
+  }
+  draw() {
+    game.ctx.beginPath();
+    game.ctx.fillStyle = this.color;
+    game.ctx.rect(this.x, this.y, this.width, this.height);
+    game.ctx.fill();
+    game.ctx.closePath();
+  }
+  collision(otherobj) {
+    let crash = true;
+    if ((this.y + (this.height) < otherobj.y) || (this.y > otherobj.y + (otherobj.height)) ||
+      (this.x + (this.width) < otherobj.x) || (this.x > otherobj.x + (otherobj.width))) {
+      crash = false;
+    }
+    return crash;
+  }
+  collisionC(otherobj) {
+    let crash = false;
+    let dx = this.x - otherobj.x;
+    let dy = this.y - otherobj.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < this.width/2 + otherobj.width/2) {
+      crash = true;
+    }
+    return crash
+  }
+}
+//// ITEM
+class Item extends Object {
+  constructor(x, y, width, height, color, dx, dy, img, type) {
+    super(x, y, width, height, color, dx, dy);
+    this.img = img;
+    this.type = type;
+  }
+  draw() {
+    game.ctx.beginPath();
+    game.ctx.fillStyle = this.color;
+    game.ctx.rect(this.x, this.y, this.width, this.height);
+    //game.ctx.fill();
+    game.ctx.drawImage(this.img.set, this.img.sx, this.img.sy, this.img.sw, this.img.sh, Math.round(this.x-this.width*1.5/6), Math.round(this.y), this.width*1.5, this.height)
+    game.ctx.closePath();
+  }
+}
+//// PLAYER
+class Player extends Object {
+  constructor(x, y, width, height, color, img) {
+    super(x, y, width, height, color, img);
+    this.power = 1;
+    this.typeImg = getRndInteger(0,3)*17;
+    this.img = {set:animals,sx:15,sy:80+this.typeImg,sw:15,sh:17};
+
+    this.alive = true;
+    this.invulnerable = false;
+    this.invulnerableTimer = 0;
+    this.charge = 3;
+    this.hp = 3;
+    this.powerImg = {set:foods,sx:107,sy:0,sw:13,sh:13};
+    this.bombImg = {set:foods,sx:129,sy:0,sw:22,sh:22};
+    this.hpImg = {set:foods,sx:120,sy:0,sw:9,sh:14};
+
+    this.saiyanImg = {set:saiyan,sx:0,sy:0,sw:84,sh:102};
+    this.saiyanTimer = 0;
+  }
+  draw() {
+    if (game.endlessMode) this.drawSaiyan();
+    if (this.alive) this.drawCharacter()
+    this.drawUI();
+  }
+  drawCharacter() {
+    this.imgX = this.x-this.width*4/2;
+    this.imgY = this.y-this.width*3;
+    this.imgW = this.width*4;
+    this.imgH = this.height*5;
+    game.ctx.beginPath();
+    game.ctxUI.fillStyle = this.color;
+    game.ctxUI.arc(this.x, this.y, this.width / 2, 0, 2 * Math.PI);
+    game.ctxUI.fill();
+    game.ctx.drawImage(this.img.set, this.img.sx, this.img.sy, this.img.sw, this.img.sh, Math.round(this.imgX), Math.round(this.imgY), this.imgW, this.imgH)
+    if (this.invulnerable) this.invulnerableDraw()
+    game.ctx.closePath();
+  }
+  invulnerableDraw() {
+    this.invulnerableTimer += 1;
+    if (this.invulnerableTimer == 3) {
+      this.invulnerableTimer = 0;
+      game.ctx.clearRect(this.imgX, this.imgY, this.imgW, this.imgH);
+    }
+  }
+  drawSaiyan() {
+    this.saiyanTimer += 1;
+    if (this.saiyanTimer == 10) {
+      this.saiyanTimer = 0;
+      this.saiyanImg.sx += 84;
+    }
+    this.saiyanImg.sx = this.saiyanImg.sx%(84*4);
+    game.ctx.drawImage(this.saiyanImg.set, this.saiyanImg.sx, this.saiyanImg.sy, this.saiyanImg.sw, this.saiyanImg.sh, Math.round(this.x-this.width*7), Math.round(this.y-this.width*15), this.width*14, this.width*17)
+  }
+  drawUI() {
+    let UIw = 20
+    game.ctxUI.beginPath();
+    for (let i = 0; i < Math.floor(this.hp); i++) {
+      game.ctxUI.drawImage(this.hpImg.set, this.hpImg.sx, this.hpImg.sy, this.hpImg.sw, this.hpImg.sh, Math.round(0+(UIw+5)*i), game.canvasUI.height-45, UIw, UIw)
+    }
+    for (let i = 0; i < Math.floor(this.power); i++) {
+      game.ctxUI.drawImage(this.powerImg.set, this.powerImg.sx, this.powerImg.sy, this.powerImg.sw, this.powerImg.sh, Math.round(0+(UIw+5)*i), game.canvasUI.height-20, UIw, UIw)
+    }
+    for (let i = 1; i <= Math.floor(this.charge); i++) {
+      game.ctxUI.drawImage(this.bombImg.set, this.bombImg.sx, this.bombImg.sy, this.bombImg.sw, this.bombImg.sh, Math.round(game.canvasUI.width-(UIw+5)*i), game.canvasUI.height-45, UIw, UIw)
+    }
+    game.ctxUI.closePath();
+  }
+  dead() {
+    if (this.hp < 1 && gameStarted) {
+      gameStarted = false;
+      console.log('GAME OVER');
+      menu();
+      bgwav.load();
+      bgwav.pause();
+      bosswav.load();
+      bosswav.pause();
+      setTimeout(() => {
+        document.location.reload(true)
+      },2000);
+    }
+    this.hp--;
+    this.alive = false;
+    this.x = -10;
+    this.y = -10;
+  }
+  revive() {
+    if (!this.alive) {
+      detonate();
+      this.x = game.startX;
+      this.y = game.startY;
+      this.alive = true;
+      this.invulnerable = true;
+      setTimeout(() => this.invulnerable = false,4000);
+    }
+  }
+  getItem(otherobj) {
+    let crash = true;
+    if ((this.imgY + (this.imgH) < otherobj.y) || (this.imgY > otherobj.y + (otherobj.height)) ||
+      (this.imgX + (this.imgW) < otherobj.x) || (this.imgX > otherobj.x + (otherobj.width))) {
+      crash = false;
+    }
+    return crash;
+  }
+}
+//// BULLETS
+// Circle bullet
+class bulletC extends Object {
+  draw() {
+    game.ctx.beginPath();
+    //game.ctx.fillStyle = "black";
+    //game.ctx.arc(this.x, this.y, this.width / 2, 0, 2 * Math.PI);
+    //game.ctx.fill();
+    game.ctx.drawImage(this.img.set, this.img.sx, this.img.sy, this.img.sw, this.img.sh, Math.round(this.x-this.width/2), Math.round(this.y-this.width/2), this.width, this.width)
+    game.ctx.closePath();
+  }
+}
+// Rect bullet
+class bulletPlayer extends Object {
+  draw() {
+    game.ctx.beginPath();
+    game.ctx.fillStyle = this.color;
+    //game.ctx.arc(this.x, this.y, this.width / 2, 0, 2 * Math.PI);
+    game.ctx.drawImage(this.img.set, this.img.sx, this.img.sy, this.img.sw, this.img.sh, Math.round(this.x-this.width/2), Math.round(this.y-this.width/2), this.width, this.width)
+    game.ctx.fill();
+    game.ctx.closePath();
+  }
+}
+// Beam
+class Beam extends bulletC {
+  constructor(x, y, width, height, color, dx, dy, dmg, img) {
+    super(x, y, width, height, color, dx, dy, dmg, img);
+    this.img = {set:bluebeam,sx:0,sy:0,sw:25,sh:100}
+    this.timer = 0;
+
+  }
+  draw() {
+    //this.img = {set:bluebeam,sx:0,sy:0,sw:25,sh:100}
+    this.timer += 1;
+    if (this.timer == 10) {
+      this.timer = 0;
+      this.img.sx += 25;
+    }
+    this.img.sx = this.img.sx%175;
+    this.x = game.player.x-game.player.width;
+    this.y = game.player.y;
+    let height = game.canvas.height - (game.canvas.height-this.y);
+    game.ctx.beginPath();
+    game.ctx.fillStyle = this.color;
+    game.ctx.drawImage(this.img.set, this.img.sx, this.img.sy, this.img.sw, this.img.sh, Math.round(game.player.x-game.player.width/6-this.width), Math.round(game.player.y-game.canvas.height), this.width*2, game.canvas.height)
+    game.ctx.rect(game.player.x-game.player.width/6-this.width/2, game.player.y, this.width, -height);
+    //game.ctx.stroke();
+    game.ctx.closePath();
+  }
+  collision(otherobj) {
+    let crash = true;
+    if ((this.x + (this.width) < otherobj.x) || (this.x > otherobj.x + otherobj.width)) {
+      crash = false;
+    }
+    return crash;
+  }
+}
+// Bullet 2
+class bulletC2 extends bulletC {
+  draw() {
+    super.draw();
+    if (this.swap) {
+      this.dx += 1;
+    } else {
+      this.dx -= 1;
+    }
+    if (this.dx >= 4) {
+      this.swap = false;
+    } else if (this.dx <= -4) {
+      this.swap = true;
+    }
+  }
+}
+// Bullet 6
+class bulletC6 extends bulletC {
+  draw() {
+    super.draw();
+  }
+}
+class bulletC7 extends bulletC {
+  draw() {
+    //pg.draw.circle(screen, WHITE, (int(self.r * pl.cos(self.angle) + \
+    //                    self.x), int(self.r * pl.sin(self.angle) + self.y)), 3)
+    this.r += 0.1;
+    this.x = this.r * (this.dx / speed) + this.x
+    this.y = this.r + (this.dy / speed) + this.y
+    //console.log(this.a)
+    game.ctx.beginPath();
+    game.ctx.fillStyle = this.color;
+    game.ctx.arc(this.x, this.y, this.width / 2, 0, 2 * Math.PI);
+    game.ctx.fill();
+    game.ctx.closePath();
+  }
+}
+//// ENEMY
+class Enemy {
+  constructor(x, y, width, height, color, dx = 0, dy = 0, type = 0, bulletType = 0, hp = 5) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.dx = dx;
+    this.dy = dy;
+    this.type = type;
+    this.bulletType = bulletType;
+    this.hp = hp;
+
+    this.typeImg = getRndInteger(1,5)%4 * 20
+    this.img = {set:animals,sx:15,sy:this.typeImg,sw:15,sh:20};
+    this.shootCDMaks = 300;
+    this.shootCD = this.shootCDMaks/2;
+    this.swap = false;
+  }
+  draw() {
+    this.checkMovement();
+    game.ctx.beginPath();
+    game.ctx.fillStyle = this.color;
+    game.ctx.rect(this.x, this.y, this.width, this.height);
+    //game.ctx.fill();
+    game.ctx.drawImage(this.img.set, this.img.sx, this.img.sy, this.img.sw, this.img.sh, Math.round(this.x), Math.round(this.y-this.height*1.5/3), this.width, this.height*1.5)
+    game.ctx.closePath();
+    //console.log(this.shootCD)
+    if (this.shootCD !== this.shootCDMaks) this.shootCD++;
+  }
+  checkMovement() {
+    if (this.dx > 0) {
+      this.img = {set:animals,sx:30,sy:this.typeImg,sw:15,sh:20};
+    } else if (this.dx < 0) {
+      this.img = {set:animals,sx:0,sy:this.typeImg,sw:15,sh:20};
+    } else {
+      this.img = {set:animals,sx:15,sy:this.typeImg,sw:15,sh:20};
+    }
+  }
+  shoot(arr) {
+    let speed = 4;
+    if (this.bulletType == 1) {
+      bullet1(arr, this.x+this.width/2, this.y, 15, "red", speed);
+    } else if (this.bulletType == 2) {
+      bullet2(arr, this.x+this.width/2, this.y, 15, "red", speed);
+    } else if (this.bulletType == 3) {
+      bullet3(arr, this.x+this.width/2, this.y, 15, "red", speed);
+    } else if (this.bulletType == 4) {
+      bullet1(arr, this.x+this.width/2, this.y, 15, "red", speed);
+      bullet3(arr, this.x+this.width/2, this.y, 15, "red", speed);
+    } else if (this.bulletType == 5) {
+      bullet4(arr, this.x+this.width/2, this.y, 15, "red", speed);
+    } else if (this.bulletType == 6) {
+      bullet5(arr, this.x+this.width/2, this.y, 15, "red", speed);
+    }
+  }
+  checkType() {
+    if (this.type == 2) {
+      this.dx += 0.005;
+    } else if (this.type == 3) {
+      this.dx -= 0.005;
+    } else if (this.type == 4) {
+      if (this.dy > 0 && this.y >= game.canvas.height/4) {
+        this.dy = 0;
+        setTimeout(() => this.dy -= 1.1 ,3000)
+      }
+    } else if (this.type == 5) {
+      if (this.swap) {
+        this.dx += 0.1;
+      } else {
+        this.dx -= 0.1;
+      }
+      if (this.dx >= 5) {
+        this.swap = false;
+      } else if (this.dx <= -5) {
+        this.swap = true;
+      }
+    }
+  }
+}
+// Boss
+class Boss extends Enemy{
+  constructor(x, y, width, height, color, dx, dy, type, bulletType, hp) {
+    super(x, y, width, height, color, dx, dy=2, type, bulletType, hp=300)
+    this.startHp = this.hp;
+    // normal shoot
+    this.shootCDMaks = 100000;
+    this.shootCD = 0;
+    // left and right move
+    this.swap = true;
+    // timer for swap
+    this.maxSwapTime = 300;
+    // timer for shoot
+    this.maxSwapShoot = 300/4;
+    this.swapTime = 0;
+    this.hpImg = {set:foods,sx:0,sy:33,sw:178,sh:45};
+    this.barPointerImg = {set:foods,sx:0,sy:0,sw:50,sh:23};
+  }
+  hpBar() {
+    let x = 65, y = 17;
+    let max = (game.canvas.width-2*x)/this.startHp;
+    let length = this.hp*max;
+    game.ctx.beginPath();
+    game.ctx.fillStyle = 'black';
+    game.ctx.rect(x, y, game.canvas.width-2*x, 20);
+    game.ctx.fill();
+    game.ctx.closePath();
+    game.ctx.beginPath();
+    game.ctx.fillStyle = 'red';
+    game.ctx.rect(x, y, length, 20);
+    game.ctx.fill();
+    game.ctx.drawImage(this.hpImg.set, this.hpImg.sx, this.hpImg.sy, this.hpImg.sw, this.hpImg.sh, 0, -10, game.canvas.width, 70)
+    game.ctx.closePath();
+  }
+  bossPointer() {
+    game.ctx.beginPath();
+    game.ctx.fillStyle = 'black';
+    game.ctx.rect(this.x+this.width/2-30/2, game.canvas.height-12, 30, 10);
+    //game.ctx.fill();
+    game.ctxUI.drawImage(this.barPointerImg.set, this.barPointerImg.sx, this.barPointerImg.sy, this.barPointerImg.sw, this.barPointerImg.sh, Math.round(this.x+this.width/2-30/2), game.canvasUI.height-70, 40, 20)
+    game.ctx.closePath();
+  }
+  draw() {
+    // BOSS ??????
+    super.draw();
+    if (this.x <= this.width+10) {
+      this.dx = 1;
+      setTimeout(() => this.dx = 0,60);
+    }
+    if (this.x >= game.canvas.width-this.width-10) {
+      this.dx = -1;
+      setTimeout(() => this.dx = 0,60);
+    }
+
+    if (this.y >= game.canvas.height/6) this.dy = 0;
+    if (this.swapTime%this.maxSwapShoot == 0 && this.dy == 0 && this.swap) {
+      this.dx = 2*getRndInteger(-1,1);
+    }
+    if (this.swapTime == this.maxSwapTime) {
+      if (this.swap) {
+        setTimeout(() => {
+          bullet6(game.enemyBullet, this.x+this.width/2, this.y+this.height/2, 15, "red", 2, 30);
+          this.dx = 0;
+        },200);
+        this.swap = false;
+        this.swapTime = -this.maxSwapTime/3;
+      } else {
+        this.swap = true;
+        this.swapTime = -this.maxSwapTime/3;
+      }
+    }
+    if (this.swapTime !== this.maxSwapTime) this.swapTime++;
+    //console.log(this.swapTime)
+  }
+}
+//// OTHERS
+class Splatter extends bulletC {
+  constructor(x, y, width, height, color, dx = 0, dy = 0, swap = false, r = 0, lifeTime, img) {
+    super(x, y, width, height, color, dx, dy, swap, r, img);
+    this.lifeTime = lifeTime;
+    this.img = {set:sprite1,sx:435,sy:298,sw:23,sh:23}
+    this.img.sx = getRndInteger(0,9)*23 + 435
+    this.width *= 1.5
+    this.x += this.width
+    this.y += this.width
+  }
+  draw() {
+    if (this.lifeTime !== 0) {
+      this.lifeTime--;
+    }
+    super.draw();
+  }
+}
---
+++ projects/projectCat/js/main.js
@@ -0,0 +1,230 @@
+//// LOADS
+let gameLoaded = false;
+let loadedImg = 0;
+
+const canvasBg = document.getElementById("bg");
+const ctxBg = canvasBg.getContext("2d");
+ctxBg.webkitImageSmoothingEnabled = false;
+ctxBg.mozImageSmoothingEnabled = false;
+ctxBg.imageSmoothingEnabled = false;
+//// IMAGE
+// Background
+const bg = new Image();
+bg.onload = () => {
+    loadedImg++;
+    checkLoaded();
+};
+bg.src = './img/night.png';
+let bg1Y = 0;
+let bg2Y = 600;
+let bgScrollSpeed = 3;
+function drawBg() {
+  if (game.screenShake) {
+    ctxBg.save();
+    let dx = Math.random()*5;
+    let dy = Math.random()*2;
+    ctxBg.translate(dx, dy);
+  }
+  bg1Y += bgScrollSpeed;
+  bg2Y += bgScrollSpeed;
+  if (bg1Y >= 595) bg1Y = -595;
+  if (bg2Y >= 595) bg2Y = -595;
+  //console.log(bg1Y,bg2Y)
+  ctxBg.clearRect(0,0,canvasBg.width,canvasBg.height);
+  ctxBg.drawImage(bg,0,0,canvasBg.width,canvasBg.height,0,bg1Y,canvasBg.width*3.35,canvasBg.height*4);
+  ctxBg.drawImage(bg,0,0,canvasBg.width,canvasBg.height,0,bg2Y,canvasBg.width*3.35,canvasBg.height*4);
+  if (game.screenShake) {
+    ctxBg.restore();
+  }
+}
+
+// title
+const titleFrame = new Image();
+titleFrame.onload =() => {
+    loadedImg++;
+    checkLoaded();
+};
+titleFrame.src = './img/frame.png';
+
+// titlebg
+const titleBg = new Image();
+titleBg.onload =() => {
+    loadedImg++;
+    checkLoaded();
+};
+titleBg.src = './img/titlebg.png';
+
+// Shots???
+const sprite1 = new Image();
+sprite1.onload =() => {
+    loadedImg++;
+    checkLoaded();
+};
+sprite1.src = './img/sprite1.png';
+
+// Laserbeam
+const bluebeam = new Image();
+bluebeam.onload = () => {
+    loadedImg++;
+    checkLoaded();
+};
+bluebeam.src = './img/pinkbeam.png';
+
+// Animals (20/17x15)
+const animals = new Image();
+animals.onload = () => {
+    loadedImg++;
+    checkLoaded();
+};
+animals.src = './img/animals.png';
+
+// Items
+const foods = new Image();
+foods.onload = () => {
+    loadedImg++;
+    checkLoaded();
+};
+foods.src = './img/items.png';
+
+// saiyan glow
+const saiyan = new Image();
+saiyan.onload = () => {
+    loadedImg++;
+    checkLoaded();
+};
+saiyan.src = './img/saiyan.png';
+
+
+
+//// Music
+let loadedMusic = 0;
+let bgVolume = 0.5;
+// Background wav
+const bgwav = new Audio('./sfx/bgm.mp3');
+bgwav.addEventListener('canplaythrough', () => {
+  if (!gameLoaded) {
+    bgwav.volume = bgVolume;
+    bgwav.loop = true;
+    loadedMusic++;
+    checkLoaded();
+  }
+}, false);
+
+// boss wav
+const bosswav = new Audio('./sfx/boss.WAV');
+bosswav.addEventListener('canplaythrough', () => {
+  if (!gameLoaded) {
+    loadedMusic++;
+    checkLoaded();
+  }
+}, false);
+
+// enemydead wav
+const enemyDeadwav = new Audio('./sfx/enemydead.wav');
+enemyDeadwav.addEventListener('canplaythrough', () => {
+  if (!gameLoaded) {
+    enemyDeadwav.volume = 0.1;
+    loadedMusic++;
+    checkLoaded();
+  }
+}, false);
+
+// dead wav
+const deadwav = new Audio('./sfx/dead.wav');
+deadwav.addEventListener('canplaythrough', () => {
+  if (!gameLoaded) {
+    deadwav.volume = 0.1;
+    loadedMusic++;
+    checkLoaded();
+  }
+}, false);
+
+// atk wav
+const atkwav = new Audio('./sfx/atk.wav');
+atkwav.addEventListener('canplaythrough', () => {
+  if (!gameLoaded) {
+    atkwav.volume = 0.017;
+    loadedMusic++;
+    checkLoaded();
+  }
+}, false);
+
+// power wav
+const powerwav = new Audio('./sfx/power.wav');
+powerwav.addEventListener('canplaythrough', () => {
+  if (!gameLoaded) {
+    powerwav.volume = 0.9;
+    loadedMusic++;
+    checkLoaded();
+  }
+}, false);
+
+// alert wav
+const alertwav = new Audio('./sfx/alert.wav');
+alertwav.addEventListener('canplaythrough', () => {
+  if (!gameLoaded) {
+    alertwav.volume = 0.4;
+    loadedMusic++;
+    checkLoaded();
+  }
+}, false);
+
+function checkLoaded() {
+  if (loadedImg == 8 && loadedMusic == 7) {
+    console.log(`Loaded total tilesets: ${loadedImg}`);
+    console.log(`Loaded total music: ${loadedMusic}`);
+    gameLoaded = true;
+    setTimeout(() => {
+      menu();
+    },50)
+  }
+}
+function moveMenu(x,dir) {
+  chooseMeny[x] = 0;
+  if (dir == 'down') {
+    if (x+1 == chooseMeny.length) {
+      chooseMeny[0] = 1;
+    } else {
+      chooseMeny[x+1] = 1;
+    }
+  } else {
+    if (x-1 == -1) {
+      chooseMeny[chooseMeny.length-1] = 1;
+    } else {
+      chooseMeny[x-1] = 1;
+    }
+  }
+}
+function menyChoose(x) {
+  if (x == 0) {
+    console.log('start game');
+    gameStarted = true;
+    setTimeout(() => {
+      bgwav.play();
+      detonate();
+      draw();
+      document.getElementById('none').style.backgroundColor = 'black';
+    },100);
+
+  } else if (x == 1) {
+    console.log('controls');
+
+  } else {
+    console.log('help');
+  }
+}
+let gameStarted = false;
+let chooseMeny = [1,0,0];
+function menu() {
+  game.ctxUI.clearRect(0,0,game.canvasUI.width,game.canvasUI.height);
+  game.ctxUI.drawImage(titleBg,0,0,188,300,30,30,game.canvasUI.width-50,game.canvasUI.height-50);
+  game.ctxUI.drawImage(titleFrame,0,0,64,100,0,0,game.canvasUI.width,game.canvasUI.height);
+  let titleX = game.canvas.width/2;
+  let titleY = 150;
+  game.ctxUI.drawImage(titleFrame,64,0+(9*chooseMeny[0]),75,9,titleX,titleY, 150,20);
+  game.ctxUI.drawImage(titleFrame,64,18+(9*chooseMeny[1]),75,9,titleX,titleY+30, 150,20);
+  game.ctxUI.drawImage(titleFrame,64,36+(9*chooseMeny[2]),75,9,titleX,titleY+60, 150,20);
+  let pointer = chooseMeny.indexOf(1);
+  if (pointer !== -1) game.ctxUI.drawImage(titleFrame,64,54,5,7,titleX-20,titleY+(30*pointer)+2, 15,15);
+  if (!gameStarted) requestAnimationFrame(menu);
+}
