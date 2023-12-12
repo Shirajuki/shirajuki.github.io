0c9795dbad0ddaba39b32f568046312704406a89
Jonny Ngo Luong <shirajuki00@gmail.com>
2023-12-12 22:07:33 +0100

    feat: initialize new page, end of 2023 design

--- src/lib/components/PostList.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import type PostType from '$lib/posts/_post.svelte';
-  import Post from './Post.svelte';
-  export let posts: PostType[];
-</script>
-
-{#each posts as post}
-  <Post {post} />
-{/each}
--- src/lib/utils.ts
+++
@@ -1,3 +0,0 @@
-export const displayDate = (date: Date) => {
-  return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-};
