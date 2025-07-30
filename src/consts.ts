e9c4f7e0ad4a8322cfe0f69f04ef546df7f9a937
Jonny Ngo Luong <jonny.luong@omegapoint.no>
2025-07-30 15:41:51 +0200

    chore: add tags and final fixes

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,7 @@
 export const SITE_AUTHOR = "Shirajuki";
 export const SITE_DESCRIPTION =
   "Hello. This is my digital garden. I may document my discoveries, cheatsheets, solutions and thought process to CTF challenges as well as all things in general in the form of writeups here.";
+export const NAV_LINKS = [
+  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/about" },
+];
--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -2,10 +2,49 @@ import { getCollection, type CollectionEntry } from "astro:content";
 
 export async function getAllPosts({
   limit,
-}: { limit?: number } = {}): Promise<CollectionEntry<"blog">[]> {
+}: {
+  limit?: number;
+} = {}): Promise<CollectionEntry<"blog">[]> {
   const posts = await getCollection("blog");
   const filteredPosts = posts
     .filter((post) => post.data.draft !== true)
     .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
   return limit ? filteredPosts.slice(0, limit) : filteredPosts;
+}
+
+export type Tag = {
+  tag: string;
+  count: number;
+};
+export async function getAllTags(): Promise<Tag[]> {
+  const posts = await getCollection("blog");
+  const filteredPosts = posts
+    .filter((post) => post.data.draft !== true)
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+  const tags: { [key: string]: number } = {};
+  filteredPosts.forEach((post) => {
+    post.data.tags?.forEach((tag) => {
+      if (!tags[tag]) {
+        tags[tag] = 1;
+      }
+      tags[tag]++;
+    });
+  });
+  const sortedTags = Object.entries(tags).sort((a, b) => b[1] - a[1]);
+  return sortedTags.map(([tag, count]) => ({ tag, count }));
+}
+
+export async function getPostsByTag({
+  tag,
+}: {
+  tag?: string;
+}): Promise<CollectionEntry<"blog">[]> {
+  const posts = await getAllPosts();
+  if (!tag) {
+    return [];
+  }
+  const filteredPosts = posts.filter((post) => post.data.tags?.includes(tag));
+  return filteredPosts
+    .filter((post) => post.data.draft !== true)
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 }
