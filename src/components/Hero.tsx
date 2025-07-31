d94b3049f74e43944b7ab7a6a4aec87f9748f8cb
Jonny Ngo Luong <jonny.luong@omegapoint.no>
2025-07-31 09:36:51 +0200

    chore: fix dvh

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,16 +15,18 @@ const heroAnimate = {
 
 const Hero = () => {
   return (
-    <section class="relative bg-primary w-full top-0 left-0 h-screen flex justify-center items-center text-center -translate-y-16 overflow-hidden">
+    <section class="relative bg-primary w-full top-0 left-0 h-svh min-h-screen flex justify-center items-center text-center -translate-y-16 overflow-hidden">
       <motion.article
         class="relative sm:w-7/12 w-full text-2xl z-20 max-w-lg"
         transition={{ staggerChildren: 2.5, delayChildren: 2.8 }}
       >
         <motion.header class="fancy-font text-foreground" {...heroAnimate}>
-          <h2>Hello, i’m</h2>
+          <h2>Hello, I’m</h2>
         </motion.header>
         <motion.section {...heroAnimate}>
-          <h1 class="fancy-font sm:text-8xl text-7xl font-bold py-4 text-white">Shirajuki</h1>
+          <h1 class="fancy-font sm:text-8xl text-7xl font-bold py-4 text-white">
+            Shirajuki
+          </h1>
         </motion.section>
         <motion.footer {...heroAnimate}>
           <h2 class="fancy-font text-foreground w-11/12 mx-auto">
